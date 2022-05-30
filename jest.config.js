ee5c6835da9658690771420fa2c1194d4d1e8fbe
AnthonyBuisset <43467246+AnthonyBuisset@users.noreply.github.com>
2022-05-30 14:14:25 +0200

    ci: github workflow for tests and code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      statements: 100,
+      branches: 100,
+      functions: 100,
+    }
+  }
 };
